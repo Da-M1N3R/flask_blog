72a4c129ba1e03b696a4ae484e9ed1ad74f82403
Frog <uranium322win@gmail.com>
2023-07-24 01:03:20 +0800

    Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Kim Chaewon',
+        'title': 'LE SSERAFIM - Lead Singer',
+        'content': 'Darwin, you are discipline, relentless & unpredictable. You will be a fine young man.',
+        'date_posted': 'July 21, 2023',
+    },
+    {
+        'author': 'Kok Jia Wei',
+        'title': 'Nestle - Food Science Engineer',
+        'content': 'Darwin, you will be rich. With your work ethic and mindset, nothing can stand in your way. Remember that.',
+        'date_posted': 'March 22, 2023',
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
